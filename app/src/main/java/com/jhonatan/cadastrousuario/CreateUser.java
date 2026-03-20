3975689b38586f41210ba009fa8baf860832ada8
SrJhol <mendes.jhonatan039@gmail.com>
2026-03-19 21:51:57 -0300

    2 Parte

--- app/src/main/java/com/jhonatan/cadastrousuario/CreateUser.java
+++ app/src/main/java/com/jhonatan/cadastrousuario/CreateUser.java
@@ -1,5 +1,8 @@
 package com.jhonatan.cadastrousuario;
 //BLOCO DE IMPORTAÇÃO
+import android.os.Bundle;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.textfield.TextInputEditText;
@@ -7,6 +10,27 @@ import com.google.android.material.textfield.TextInputEditText;
 public class CreateUser extends AppCompatActivity {
     TextInputEditText editNome, editEmail;
     Button btnSalvar;
+
+    @Override
+    protected  void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_user);
+
+        editNome = findViewById(R.id.nome_completo);
+        editEmail = findViewById(R.id.email);
+        btnSalvar = findViewById(R.id.btnSalvar);
+
+        btnSalvar.setOnClickListener( v -> {
+            String nome = editNome.getText().toString();
+            String email = editEmail.getText().toString();
+
+            if (!nome.isEmpty()){
+                MainActivity.listaNomes.add(nome + "(" + email +")");
+                finish();;
+            }
+        });
+
+    }
 
 
 }
