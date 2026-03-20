2cde00116bee7bca0566dffa09dd146954a27641
SrJhol <mendes.jhonatan039@gmail.com>
2026-03-19 21:27:48 -0300

    Adicionando novos itens

---
+++ app/src/main/java/com/jhonatan/cadastrousuario/CreateUser.java
@@ -0,0 +1,12 @@
+package com.jhonatan.cadastrousuario;
+//BLOCO DE IMPORTAÇÃO
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+public class CreateUser extends AppCompatActivity {
+    TextInputEditText editNome, editEmail;
+    Button btnSalvar;
+
+
+}
---
+++ app/src/main/java/com/jhonatan/cadastrousuario/UserAdapter.java
@@ -0,0 +1,4 @@
+package com.jhonatan.cadastrousuario;
+
+public class UserAdapter {
+}
